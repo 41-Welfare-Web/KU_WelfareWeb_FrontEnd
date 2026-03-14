bbac1068669eab3a24ad4c03bcd76bfcb06ef0c0
dododokk <kdoeun05@konkuk.ac.kr>
2026-03-15 02:36:00 +0900

    [fix] CI 에러 해결

--- src/features/Partnership/Partnership.tsx
+++ src/features/Partnership/Partnership.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import PartnershipTabs, {
